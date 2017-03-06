3d79e5e6754a3a8b2c567c3a5a5b453b82f7db13
luuzun <luuzun@naver.com>
2017-03-06 11:01:47 +0900

    Added output Tell his world

--- Hello.java
+++ Hello.java
@@ -1,6 +1,7 @@
 public class Hello{
 	public static void main(String[] args){
 		System.out.println("Hellow world");	
-		System.out.println("Tell your world");
+		System.out.println("Tell your World");
+		System.out.println("Tell his World");
 	}
 }
